d99d371d3b2b0ac3ff2342b6576c10bc2d954b50
Kyllian Aloy <aloykyllian31520@gmail.com>
2023-01-26 18:06:17 +0100

    ajout du recup chemin

--- src/note.c
+++ src/note.c
@@ -2,13 +2,23 @@
 #include <stdio.h>  
 #include <string.h>
 
+typedef char *String ;
+
 typedef struct
 {
     int ID;
     float note;
 }eleve_typ;
     
-typedef char *String ;
+
+typedef struct
+{
+    eleve_typ *eleve;
+    char controle[9];
+    char nom[20];
+}matiere_typ;
+
+
 
 void virgule_vers_point(String note_lu)
 {
@@ -25,36 +35,16 @@ void virgule_vers_point(String note_lu)
 }
 
     
-void lire_fichier (char* chemin_fichier){
-    
-    eleve_typ *eleve;
-    eleve = (eleve_typ*)malloc(40 * sizeof(*eleve));
-
-    int dfsid;
-    int cpt;
-    // cpt=0;
-    //while(cpt<40)
-    //{
-        // printf("i: %d ",cpt);
-        // fflush(stdout);
-        // eleve[cpt].ID=0;
-        // eleve[cpt].note=0;
-        // cpt++;
-    //}
-
-     // for(int cpt=0;cpt<40;cpt++)
-    //{
-        //printf("i: %d ",y);
-        //fflush(stdout);
-        //eleve[y].ID=0;
-        //eleve[y].note=0;
-    //}
-    
+matiere_typ lire_fichier (char* chemin_fichier){
+    matiere_typ matiere;
+    strcpy(matiere.controle,"rien");
+    //eleve_typ *eleve;
+    matiere.eleve = (eleve_typ*)malloc(40 * sizeof(eleve_typ));
 
     FILE* fichier = NULL;//pointeur de fichier pour utiliser les fonction associé
     char mot_lu[700]="";
     char ID_lu[700]="";
-    String note_lu;
+    char note_lu[100]="";
     int nb_eleve=0;
 
 
@@ -64,32 +54,35 @@ void lire_fichier (char* chemin_fichier){
     }
 
 
-    for(;strcmp(mot_lu,"22212673")!=0;fscanf(fichier,"%s",&mot_lu))
+    for(int cot=0;strcmp(mot_lu,"22212673")!=0;fscanf(fichier,"%s",&mot_lu))
     {
-        
 
-        //printf("%s  ",mot_lu);
+        if(strncmp(mot_lu,"ELU",3)==0)
+        {
+        strcpy(matiere.nom,mot_lu);
+        }        
+        else if(strncmp(mot_lu,"CC",2)==0)
+        {
+
         //fflush(stdout);
+         strcpy(matiere.controle,mot_lu);
+         }    
+        // printf("mot : %s\n",mot_lu);
+        // fflush(stdout);
+
     }
-    char jsp[100];
-                printf("la");
-        fflush(stdout);
-    fscanf(fichier,"%s",&jsp);
-                printf("la");
-        fflush(stdout);
-        virgule_vers_point(jsp);
-    eleve[nb_eleve].ID=atoi("22212673");
-    eleve[nb_eleve].note=atof(jsp);
-                printf("la");
-        fflush(stdout);
 
 
-    //printf("taille test: %s",note_lu);
+    fscanf(fichier,"%s",note_lu);
+
+
+    virgule_vers_point(note_lu);
+
+    matiere.eleve[nb_eleve].ID=atoi("22212673");
+    matiere.eleve[nb_eleve].note=atof(note_lu);
 
 
 printf("\nlecture fichier\n");
-
-//printf("%d %f\n", eleve[nb_eleve].ID, eleve[nb_eleve].note);
 
     for(nb_eleve=1;strcmp(ID_lu,"MOYENNE")!=0;fscanf(fichier,"%s",&ID_lu))
     
@@ -101,61 +94,119 @@ printf("\nlecture fichier\n");
 
         virgule_vers_point(note_lu);
 
-        eleve[nb_eleve].ID=atoi(ID_lu);
-        eleve[nb_eleve].note=atof(note_lu);
+        matiere.eleve[nb_eleve].ID=atoi(ID_lu);
+        matiere.eleve[nb_eleve].note=atof(note_lu);
 
         if(strcmp(note_lu,"Validé")==0  || strcmp(note_lu,"DISPENSE")==0 )
         {
             fscanf(fichier,"%*s",note_lu);
-            eleve[nb_eleve].note=-1;
+            matiere.eleve[nb_eleve].note=-1;
         }
-         //printf("%d %f\n", eleve[nb_eleve].ID, eleve[nb_eleve].note);
 
         nb_eleve++;
         }
     }
-
-/*
-        for(nb_eleve=1;strcmp(ID_lu,"MOYENNE")!=0;fscanf(fichier,"%s %s\n",&ID_lu,&note_lu))
-    {
-        printf("%s %s\n",ID_lu,note_lu);
-
-        eleve[nb_eleve].ID=atoi(ID_lu);
-        eleve[nb_eleve].note=atof(note_lu);
-
-        if(strcmp(note_lu,"Validé")==0)
-        {
-            fscanf(fichier,"%*s",note_lu);
-            eleve[nb_eleve].note=-1;
-        }
-        
-
-        nb_eleve++;
- 
-        fflush(stdout);
-    }
-    */
 
     fclose(fichier);//fermeture du fichier
 
  printf("\n");
 
-for(int i=0;i<nb_eleve;i++)
-{
- printf("%d %f\n", eleve[i].ID, eleve[i].note);
+
+
+return(matiere);
+}
+
+
+void recup_chemin(){
+
+char commande[500];
+
+    strcpy(commande, "ls -ltu ");
+    strcat(commande, "../semstre_5");
+    strcat(commande, " | grep \".txt\" > fic_temp");
+    system(commande);
 
 }
 
-free(eleve);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void main (){
 
-/*lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELUMC5A2_EPS_Etudiants.txt");
-lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELUMC5A3_SHS_CC1_E.BABOURAM_Etudiants.txt");
-lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C3_OMI_GRAPHES_CC1_F.BANNAY_M.-C.LAGASQUIE_V1_Etudiants_.txt");
-lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C3_OMI_GRAPHES_CC2_F.BANNAY_M.-C.LAGASQUIE_V1_Etudiants_.txt");*/
-lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C3_OMI_UML_CC1_C.CHAUDET_V1_Etudiants.txt");
-lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C4_PROG._ORIENTEE_OBJET_CC1_M.-C.LAGASQUIE_Etudiants.txt");
+recup_chemin();
 
+matiere_typ matiere;
+matiere=lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELUMC5A2_EPS_Etudiants.txt");
+printf("nom : %s, controle : %s\n",matiere.nom,matiere.controle);
+for(int i=0;i<40;i++)
+{
+ printf("%d %f\n", matiere.eleve[i].ID, matiere.eleve[i].note);
+}
+free(matiere.eleve);
+matiere=lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELUMC5A3_SHS_CC1_E.BABOURAM_Etudiants.txt");
+printf("nom : %s, controle : %s\n",matiere.nom,matiere.controle);
+for(int i=0;i<40;i++)
+{
+ printf("%d %f\n", matiere.eleve[i].ID, matiere.eleve[i].note);
+}
+free(matiere.eleve);
+matiere=lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C3_OMI_GRAPHES_CC1_F.BANNAY_M.-C.LAGASQUIE_V1_Etudiants_.txt");
+printf("nom : %s, controle : %s\n",matiere.nom,matiere.controle);
+for(int i=0;i<40;i++)
+{
+ printf("%d %f\n", matiere.eleve[i].ID, matiere.eleve[i].note);
+}
+free(matiere.eleve);
+matiere=lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C3_OMI_GRAPHES_CC2_F.BANNAY_M.-C.LAGASQUIE_V1_Etudiants_.txt");
+printf("nom : %s, controle : %s\n",matiere.nom,matiere.controle);
+for(int i=0;i<40;i++)
+{
+ printf("%d %f\n", matiere.eleve[i].ID, matiere.eleve[i].note);
+}
+free(matiere.eleve);
+matiere=lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C3_OMI_UML_CC1_C.CHAUDET_V1_Etudiants.txt");
+printf("nom : %s, controle : %s\n",matiere.nom,matiere.controle);
+for(int i=0;i<40;i++)
+{
+ printf("%d %f\n", matiere.eleve[i].ID, matiere.eleve[i].note);
+}
+free(matiere.eleve);
+matiere=lire_fichier("../semstre_5/1A_SRI_2022_23__NOTES_ELURO5C4_PROG._ORIENTEE_OBJET_CC1_M.-C.LAGASQUIE_Etudiants.txt");
+printf("nom : %s, controle : %s\n",matiere.nom,matiere.controle);
+for(int i=0;i<40;i++)
+{
+ printf("%d %f\n", matiere.eleve[i].ID, matiere.eleve[i].note);
+}
+free(matiere.eleve);
 }
