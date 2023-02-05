d35b3a48306f55b9b9f0f3a81801d80dccbc27bd
Ryukamis <hugo.lestrade1610@gmail.com>
2023-02-05 15:43:05 +0100

    Mise en MAE

--- affichage/affichage.c
+++ affichage/affichage.c
@@ -10,43 +10,63 @@ void affichage(int numeroSemestre)
     float moy_EU3;
     int id;
     char choix;
-    do
+    int etat = 0;
+    switch(etat)
     {
+        case 0 : 
         printf("Quel est votre ID ? \n");
         fgets(id,8,stdin);
         if(sizeof(id)!=8)
         {
             printf("Mauvais ID, Veuillez Recommencer \n");
+            etat =0;
         }
-    } while (sizeof(id)!=8);
-    
-    //appel des fonctions pour récup les moyennes
+        else
+        {
+            etat = 1;
+        }
 
-    printf("Voici le récapitulatif de vos notes du Semestre %d : \n\n",numeroSemestre);
-    printf("\t Moyenne EU1 : %f \n",moy_EU1);
-    printf("\t Moyenne EU2 : %f \n",moy_EU3);
-    printf("\t Moyenne EU3 : %f \n\n",moy_EU2);
-    printf("\t Moyenne Générale : %f", moy_gene);
+        case 1:  
+        //appel des fonctions pour récup les moyennes  
+        printf("Voici le récapitulatif de vos notes du Semestre %d : \n\n",numeroSemestre);
+        printf("\t Moyenne EU1 : %f \n",moy_EU1);
+        printf("\t Moyenne EU2 : %f \n",moy_EU3);
+        printf("\t Moyenne EU3 : %f \n\n",moy_EU2);
+        printf("\t Moyenne Générale : %f", moy_gene);
+        printf("Voulez-vous plus de détails sur vos notes ? (Y/N) ou revenir à la sélection d'ID (R) ? \n");
+        fgetc(choix,1,stdin);
+        if (choix!='Y' && choix != 'y' && choix != 'N' && choix != 'n' && choix != 'R' && choix != 'r')
+        {
+            printf("Mauvais choix, Veuillez Recommencer \n");
+            etat = 1;
+        }
+        else if(choix == 'R' || choix == 'r')
+        {
+            printf("Retour à la sélection d'ID \n");
+            etat = 0;
+        }
+        else if(choix=='Y' || choix == 'y' )
+        {
+            etat = 2;
+        }
+        else if(choix=='N' || choix == 'n')
+        {
+            printf("Vous quittez le programme");
+            exit(1);
+        }
 
-do
-{
-   printf("Voulez-vous plus de détails sur vos notes ? (y/n) \n");
-   fgetc(choix,1,stdin);
-   if (choix!='y' || choix != 'n')
-   {
-    printf("Mauvais choic, Veuillez Recommencer \n");
-   }
-   
-} while (choix!='y' || choix != 'n');
-if (choix == 'n')
-{
-    printf("Vous quittez le programme \n");
-}
-else if (choix == 'y')
-{
-   // printf("")
+        case 2:
 
-}
+        //Appel fonction récup notes
+
+        // Afficher toutes les notes ou demander l'UE que l'on veut afficher
+
+
+    }
+   /* do
+    {
+        
+    } while (sizeof(id)!=8);*/
 
 
     
